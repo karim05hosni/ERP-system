fe2aa8375dd116a3e69b32833248c0d96cdf21db
Karim Hosny <kokehosny9@gmail.com>
2024-10-08 13:20:46 +0300

    v1 commit  EcommerceAPI

--- laravel/app/Http/Controllers/API/OrderController.php
+++ laravel/app/Http/Controllers/API/OrderController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Order\PlaceOrderRequest;
 use App\Http\traits\Api_Response_Trait;
+use App\Models\Inventory;
 use App\Models\Order;
 use App\Models\Product;
 use App\Models\User;
+use App\Models\Warehouse;
 use App\Notifications\OrderConfirmed;
 use Illuminate\Http\Request;
 use Illuminate\Notifications\Notification;
@@ -21,24 +23,40 @@ class OrderController extends Controller
     
     public function placeOrder(PlaceOrderRequest $request){
         $data = $request->except('products');
-        // dd(auth('sanctum')->user());
         $data['status'] = 'Pending';
         $data['price']= 0;
         $user = User::find(auth('sanctum')->user()->id);
         $order = Order::create($data);
+        $warehouse = Warehouse::where('location', $data['address'])->get('id')->first()->id;
         foreach ($request->products as $productData) {
-            # code...
+            # code
             $product = Product::find($productData['id']);
             $data['price'] += $productData['quantity'] * $product->price;
-            // check inventory
-            if ($product->quantity >= $productData['quantity']){
-                $product->quantity -= $productData['quantity'];
-                $product->save();
+            // check if product exists in warehouse
+            // dd(Inventory::where('product_id', $product->id)->where('warehouse_id', 1)->first());
+            if (Inventory::where('product_id', $product->id)->where('warehouse_id', $warehouse)->first()){
+                // check quantity in warehouse
+                if (Inventory::where('warehouse_id', $warehouse)->first()->quantity >= $productData['quantity']){
+                    // update quantity in warehouse
+                    Inventory::where('warehouse_id', $warehouse)->first()->update([
+                        'quantity' => Inventory::where('warehouse_id', $warehouse)->first()->quantity - $productData['quantity']
+                    ]);
+                } else {
+                    // check other warehouses
+                    Inventory::find('product_id', $productData['id'])->where('quantity','>',$productData['quantity']);
+                }
             } else {
-                return $this->Api_Response(message:'Product '.$product->name_en.' Sold Out !');
+                $warehouse = Inventory::where('product_id', $product->id)
+                ->where('quantity','>',$productData['quantity'])
+                ->get('warehouse_id')->first()->warehouse_id;
+                // dd($warehouse);
             }
             // record pivot table
-            $order->products()->attach($productData['id'], ['quantity' => $productData['quantity']]);
+            // check if product in warehouse
+            $order->products()->attach($productData['id'], [
+                'quantity' => $productData['quantity'],
+                'warehouse_id' => $warehouse
+            ]);
         }
         // validate payment
         $payment = true;
--- laravel/app/Models/Warehouse.php
+++ laravel/app/Models/Warehouse.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Inventory extends Model
+class Warehouse extends Model
 {
     use HasFactory;
-    protected $table = 'Inventory';
     protected $guarded = [];
+    public $timestamps = false;
 }
